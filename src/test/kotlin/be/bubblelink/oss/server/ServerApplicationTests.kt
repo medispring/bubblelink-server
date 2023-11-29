08d36e7a3af22bc456718fbf74eda5ba3e5e2aa1
Damien Libouton <damien.libouton@medispring.be>
2023-11-29 12:43:38 +0100

    fix conflict on gitignore

---
+++ src/test/kotlin/be/bubblelink/oss/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package be.bubblelink.oss.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
